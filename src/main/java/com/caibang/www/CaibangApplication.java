ecca2c0d7e9307e085d98edca44d157c11d01210
liqiong <15594952639@163.com>
2020-03-12 13:27:58 +0800

    second init

---
+++ src/main/java/com/caibang/www/CaibangApplication.java
@@ -0,0 +1,13 @@
+package com.caibang.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaibangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaibangApplication.class, args);
+    }
+
+}
