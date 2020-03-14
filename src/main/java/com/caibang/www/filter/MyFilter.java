293aca079449d66a67d2a5841a309e18534bbdf2
liqiong888 <15594952639@163.com>
2020-03-14 17:54:51 +0800

    my filter  read

--- src/main/java/com/caibang/www/filter/MyFilter.java
+++ src/main/java/com/caibang/www/filter/MyFilter.java
@@ -4,7 +4,10 @@ package com.caibang.www.filter;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 /**
  * 自定义过滤器
@@ -20,15 +23,44 @@ public class MyFilter implements Filter {
         System.out.println("--------------->过滤器被创建<----------------");
     }
 
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest httpServletRequest;
+//        StringBuilder stringBuilder = new StringBuilder();
+//        httpServletRequest = (HttpServletRequest) servletRequest;
+//        StringBuffer requestURL = httpServletRequest.getRequestURL();
+//        System.out.println("-------------->过滤器请求地址<------------" + requestURL);
+//        if (!requestURL.toString().contains("localhost:2221")) {
+//            servletRequest.getRequestDispatcher("/web_user/error").forward(servletRequest, servletResponse);
+//        } else {
+//            ServletInputStream inputStream = httpServletRequest.getInputStream();
+//            int read = inputStream.read();
+//            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+//            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//            String line = bufferedReader.readLine();
+//            while (line != null) {
+//                stringBuilder.append(line);
+//            }
+//            System.out.println("-----------params------------" + stringBuilder.toString());
+//
+//            //防止流读取一次后就没有了, 所以需要将流继续写出去
+//            httpServletRequest = (HttpServletRequest) servletRequest;
+//            ServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
+//            filterChain.doFilter(requestWrapper, servletResponse);
+//        }
+//    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletRequest httpServletRequest;
+        httpServletRequest = (HttpServletRequest) servletRequest;
         StringBuffer requestURL = httpServletRequest.getRequestURL();
         System.out.println("-------------->过滤器请求地址<------------" + requestURL);
-        if (!requestURL.toString().contains("signIn")) {
+        if (!requestURL.toString().contains("localhost:2221")) {
             servletRequest.getRequestDispatcher("/web_user/error").forward(servletRequest, servletResponse);
         } else {
-            filterChain.doFilter(servletRequest, servletResponse);
+
+            filterChain.doFilter(httpServletRequest, servletResponse);
         }
     }
 
