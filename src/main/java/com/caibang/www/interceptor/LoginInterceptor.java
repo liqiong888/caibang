7386b40f7fb0e8241018356cbeadd68797eb0090
liqiong888 <15594952639@163.com>
2020-03-15 16:43:49 +0800

    login interceptor

---
+++ src/main/java/com/caibang/www/interceptor/LoginInterceptor.java
@@ -0,0 +1,15 @@
+package com.caibang.www.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @author lq
+ * @className LoginInterceptor
+ * @description 登录拦截器
+ * @date 2020/3/1516:41
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+
+}
